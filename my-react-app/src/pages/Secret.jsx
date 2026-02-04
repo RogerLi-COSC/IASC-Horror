2758f3574cadcff5032c203bc6e17fb6b83b00b0
mistrydmpl <mistrydmpl@gmail.com>
2026-02-04 10:19:04 -0500

    updated secret page
    added content to secret page

--- my-react-app/src/pages/Secret.jsx
+++ my-react-app/src/pages/Secret.jsx
@@ -14,6 +14,47 @@ export default function Secret() {
 
         <div className="secret-box">
           [ secret content goes here ]
+          <section>
+            <ul>
+              <li><h1>OMAR KUTAY</h1></li>
+              <li><p>Age: 17</p></li>
+              <li><p>Height: 6'1</p></li>
+              <li><p>Description: Plays on river fork high football team, Jock</p></li>
+            </ul>
+          </section>
+          <section>
+            <ul>
+              <li><h1>LOGAN SANDERS</h1></li>
+              <li><p>Age: 16</p></li>
+              <li><p>Height: 5'8</p></li>
+              <li><p>Description: Wears glasses, suspenders, stripped t-shirt</p></li>
+            </ul>
+          </section>
+          <section>
+            <ul>
+              <li><h1>SASHA COWEN</h1></li>
+              <li><p>Age: 17</p></li>
+              <li><p>Height: 5'5</p></li>
+              <li><p>Description: The writer, pencil on hand, smart</p></li>
+            </ul>
+          </section>
+          <section>
+          <section>
+            <ul>
+              <li><h1>ZOEY MALANO</h1></li>
+              <li><p>Age: 17</p></li>
+              <li><p>Height: 5'6</p></li>
+              <li><p>Description: Popular, Dating Omar, Only cares about herself</p></li>
+            </ul>
+          </section>
+          <section></section>
+            <ul>
+              <li><h1>NOAH DHAVALE</h1></li>
+              <li><p>Age: 17</p></li>
+              <li><p>Height: 5'11</p></li>
+              <li><p>Description: Class clown, Drug addict, Dark clothing</p></li>
+            </ul>
+          </section>
         </div>
       </div>
     </main>
