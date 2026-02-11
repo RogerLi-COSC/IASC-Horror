af055490aec12cd6a41763703dd4f49d331faf1b
mistrydmpl <mistrydmpl@gmail.com>
2026-02-11 11:34:07 -0500

    Update Download.jsx

--- my-react-app/src/pages/Download.jsx
+++ my-react-app/src/pages/Download.jsx
@@ -1,7 +1,7 @@
 import "./Download.css";
 
 
-const targetDate = new Date("March 1, 2026 09:00:00").getTime();
+const targetDate = new Date("April 4, 2026 09:00:00").getTime();
 
 const countdown = setInterval(function () {
     const now = new Date().getTime();
